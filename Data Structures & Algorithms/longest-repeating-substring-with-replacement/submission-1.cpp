04d80f6e173758e01164f789a0276fd215817ec6
Ulyssx <2723359835@qq.com>
2026-04-05 16:11:30 +0800

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int characterReplacement(std::string s, int k) {
+        int count[26]={};
+        int res = 0;
+
+        int l = 0, maxf = 0;
+        for (int r = 0; r < s.size(); r++) {
+            count[s[r]-'A']++;
+            maxf = max(maxf, count[s[r]-'A']);
+
+            while ((r - l + 1) - maxf > k) {
+                count[s[l]-'A']--;
+                l++;
+            }
+            res = max(res, r - l + 1);
+        }
+
+        return res;
+    }
+};
